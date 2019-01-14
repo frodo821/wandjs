fde781a93f9d13336accf8c990003510f5ba6216
frodo821 <sakaic2003@gmail.com>
2019-01-14 15:27:46 +0900

    Reinitialization bug fixed.

--- src/initialize.ts
+++ src/initialize.ts
@@ -10,10 +10,13 @@ export function setup(app: string | Element, mode: AppMode) {
         Settings.instance.app_element = app;
     }
     Settings.instance.app_mode = mode;
+    let elem = Settings.instance.app_element;
 
-    if(!Settings.instance.app_element)
+    if(!elem)
         throw new WandError(`Initialization failed: ${app} is null or could not be found.`);
 
-    Settings.instance.app_element.addEventListener('wakeup', _=>register_navigator());
-    trigger(Settings.instance.app_element, 'wakeup');
+    elem.addEventListener('wakeup', _=> {
+        register_navigator();
+    });
+    trigger(elem, 'wakeup');
 }
--- src/navigation.ts
+++ src/navigation.ts
@@ -1,5 +1,7 @@
 import {load_page_async} from './pageLoader';
 
+var initialized = false;
+
 export function register_navigator() {
     document.querySelectorAll('a:not([href^="http"]):not([href^="//"]):not([data-no-rewrite])')
             .forEach(elem => {
@@ -12,14 +14,16 @@ export function register_navigator() {
                         return false;
                     });
             });
-    window.addEventListener('popstate', ev=>{
-        ev.stopPropagation();
-        ev.preventDefault();
-        navigate(location.pathname, true);
-    });
+    if(!initialized) {
+        window.addEventListener('popstate', ev=>{
+            ev.stopPropagation();
+            ev.preventDefault();
+            navigate(location.pathname, true);
+        });
+        initialized = true;
+    }
 }
 
 function navigate(url: string, onpopstate: boolean = false) {
-    console.log('navigating to '+url);
     load_page_async(url, {onpopstate: onpopstate});
 }
